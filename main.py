28ddae327fab162eed57d20eece567ee1bab56dd
Maladen-Auth <Malashway@gmail.com>
2026-01-06 17:57:54 -0500

    3: wrote word counter in main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("greetings boots")
+
+
+def get_book_text ():
+    """
+    book_path = input("Enter book file path: ")
+    with open(book_path) as book_content:
+        book = book_content.read()
+    print(book)
+    return
+    """
+
+    with open("books/frankenstein.txt") as book_content:
+        book = book_content.read()
+    # print(book)
+    
+    words = book.split()
+    print(f"Found {len(words)} total words")
+    
+    
+    return
+
+"""
+def word_count():
+    words = book.split
+    print(len(words))
+    return
+"""
+
+def main():
+    get_book_text()
+    # word_count()
+    return
+
+
+main()
+    
